58014b2bd7e1c165acf25743d8107b62bb9ca22e
azamjonabdullayev <azamjondasturchi11@gmail.com>
2023-01-25 23:00:31 +0500

    inner

--- src/pages/potpis/potpis.jsx
+++ src/pages/potpis/potpis.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import Profile from '../../components/Profile'
+import './index.scss'
 const index = ()=>{
     const [users, setUsers] = useState([])
 
@@ -18,14 +19,15 @@ const index = ()=>{
         <div className="follow w-75">
             {
                 users.length>0 ? users.map((item, index)=>{
+                    console.log(item)
                     return(
-                        <div className="cardcha d-flex justify-content-between w-100  ">
+                        <div className="carts d-flex justify-content-between w-100  " key={item.id}>
                         <div className="forimg">
                             <img src={item.avatar_url} alt="Avatar"/>
                         </div>
 
                         <div className="infolar d-flex justify-content-between align-items-center w-100 mx-2">
-                            <h5>{item.login}</h5>
+                            <a href={item.html_url} target="_blank">{item.login}</a>
                             <button className="unflow">Unfollow</button>
                         </div>
                     </div>
